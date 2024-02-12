75c6c99e9070744d8568b84293a837203a67a289
moiezshaikh18 <moiezshaikh18@gmail.com>
2024-02-13 00:34:40 +0530

    Add counter component

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,5 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Counter from "./counter";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           Learn React
         </a>
         <h1>Hello World</h1>
+        <Counter />
       </header>
     </div>
   );
---
+++ react-app/src/counter.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Counter = () => {
+  return (
+    <>
+      <h3>This is counter componet</h3>
+    </>
+  );
+};
+
+export default Counter;
