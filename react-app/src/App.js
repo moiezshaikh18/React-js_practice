914bdfe33a2e50c59e03b4b5c73a30327c14a015
mueej <v-mueej@fieldera.com>
2024-02-13 10:44:25 +0530

    Code cleanup

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,9 +3,9 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="App-header">
         <h1>Hello World</h1>
-      </header>
+      </div>
     </div>
   );
 }
