cefc08eaaac749ce0e9882ce99b69b46f4f75d86
moiezshaikh18 <moiezshaikh18@gmail.com>
2024-02-25 04:03:45 +0530

    done with the live bharat clock

--- Projects/Bharat-Clock/src/App.jsx
+++ Projects/Bharat-Clock/src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [time, setTime] = useState();
+
+  let date = new Date();
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      setTime(date.toLocaleTimeString());
+    }, 1000);
+
+    return () => {
+      clearInterval(id);
+    };
+  }, [time]);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <center
+        style={{
+          border: "10px dotted white",
+          background: "black",
+          borderRadius: "15px",
+        }}>
+        <h1 style={{ fontSize: "60px", textUnderlinePosition: "under" }}>
+          ***Bharat Clock***
+        </h1>
+        <h2>This is clock thata shows the time in Bharat at all times</h2>
+        <h3>
+          This is the current time:-{" "}
+          <h2 style={{ color: "#FFFF33" }}>
+            {date.toDateString()} - {time}
+          </h2>
+        </h3>
+      </center>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
