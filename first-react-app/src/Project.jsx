fd69e731c38a1f35b5b66137bfd8ff3f8e1bab4c
moiezshaikh18 <moiezshaikh18@gmail.com>
2024-02-25 02:53:04 +0530

    basic and added bootstrap

---
+++ first-react-app/src/Project.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Project = () => {
+  return (
+    <>
+      <div style={{ background: "white", height: "100em" }}>
+        <hr style={{ width: "800px" }} />
+        <div style={{ color: "black" }}>Project</div>
+      </div>
+    </>
+  );
+};
+
+export default Project;
--- first-react-app/src/main.jsx
+++ first-react-app/src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import Project from "./Project.jsx";
+// import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <>
+    <React.StrictMode>
+      <App />
+      {/* <Project /> */}
+    </React.StrictMode>
+  </>
+);
